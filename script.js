f4956b1dba9b782cbd5875befbfc1310b96286dd
esranuraltinay <esranuraltinay@gmail.com>
2024-11-10 22:37:44 +0300

    website

---
+++ script.js
@@ -0,0 +1,5 @@
+// Basit bir form gönderim kontrolü
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Mesajınız gönderildi!');
+});
